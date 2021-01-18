68ed9b5759e1570d39a8829c4359fb629bc13c35
alattalatta <urty5656@gmail.com>
2021-01-19 01:48:41 +0900

    initial setup

---
+++ pages/_document.tsx
@@ -0,0 +1,50 @@
+import NextDocument, { DocumentContext, DocumentInitialProps, Head, Html, Main, NextScript } from 'next/document'
+import React from 'react'
+
+import { css } from 'utils/styler'
+
+export default class Document extends NextDocument {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const originalRenderPage = ctx.renderPage
+
+    let extractedStyles: string[] = []
+
+    ctx.renderPage = () => {
+      const { styles, result } = css.getStyles(originalRenderPage)
+      extractedStyles = styles
+      return result
+    }
+
+    const initialProps = await NextDocument.getInitialProps(ctx)
+
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          {extractedStyles.map((content, index) => (
+            <style key={index} dangerouslySetInnerHTML={{ __html: content }} />
+          ))}
+        </>
+      ),
+    }
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Nanum+Gothic+Coding:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}

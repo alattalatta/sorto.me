d16e22dc0cc2a040dcf2956a12408a94408ffafa
alattalatta <urty5656@gmail.com>
2021-01-20 17:59:31 +0900

    post title

---
+++ components/Container.tsx
@@ -0,0 +1,11 @@
+import { styled } from 'utils/styler'
+
+export const CONTAINER_PADDING = 40
+
+export const Container = styled('div', {
+  marginLeft: 'auto',
+  marginRight: 'auto',
+  maxWidth: 1240,
+  paddingLeft: CONTAINER_PADDING,
+  paddingRight: CONTAINER_PADDING,
+})

200e2ebfac8db12a45f8e36274da8f0cb8e8c7e6
alattalatta <urty5656@gmail.com>
2021-02-02 20:24:18 +0900

    visitable about page

--- components/basics.tsx
+++ components/basics.tsx
@@ -11,3 +11,13 @@ export const Anchor = styled('a', {
     color: '#fff',
   },
 })
+
+export const NoScreen = styled('span', {
+  clip: 'rect(1px, 1px, 1px, 1px)',
+  height: 1,
+  margin: 0,
+  padding: 0,
+  position: 'absolute',
+  width: 1,
+  zIndex: -9,
+})

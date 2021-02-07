5f5670d1f3220e1a0716d88fc42701e46033d0c1
alattalatta <urty5656@gmail.com>
2021-02-08 03:02:55 +0900

    remove blog list from index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,46 +1,9 @@
-import fs from 'fs'
-import path from 'path'
-import util from 'util'
-
-import { GetStaticProps } from 'next'
-
-import BlogBody, { PostDatum } from 'components/BlogBody'
+import IndexBody from 'components/IndexBody'
 import { getLayout } from 'components/Layout'
-import { parsePost, POSTS_PATH, POST_FILES_PENDING } from 'utils/posts'
 import { Page } from 'utils/types'
 
-type StaticProps = { posts: readonly PostDatum[] }
-
-const Index: Page<StaticProps> = ({ posts }) => {
-  return (
-    <div>
-      <BlogBody posts={posts} />
-    </div>
-  )
+const Index: Page = () => {
+  return <IndexBody />
 }
 Index.getLayout = getLayout
 export default Index
-
-export const getStaticProps: GetStaticProps<StaticProps> = async () => {
-  const postData = await Promise.all(
-    [...(await POST_FILES_PENDING)]
-      .reverse()
-      .map((fileName) => [fileName, path.join(POSTS_PATH, fileName)])
-      .map(async ([fileName, filePath]) => {
-        const source = await util.promisify(fs.readFile)(filePath)
-
-        const { meta } = parsePost(fileName, source)
-
-        return {
-          meta,
-          slug: fileName.replace('.mdx', ''),
-        }
-      }),
-  )
-
-  return {
-    props: {
-      posts: postData,
-    },
-  }
-}

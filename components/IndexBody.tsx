06a3ae7dbf6988ea114ae8619dc0aa68c806e3fd
alattalatta <urty5656@gmail.com>
2021-02-15 02:10:12 +0900

    temporary home page

--- components/IndexBody.tsx
+++ components/IndexBody.tsx
@@ -1,11 +1,29 @@
 import React from 'react'
 
-import { Container, Anchor } from './basics'
+import { styled } from 'utils/styler'
+
+import { Anchor, Container } from './basics'
+
+const Heading = styled('h1', {
+  marginTop: 48,
+})
 
 const IndexBody: React.VFC = () => {
   return (
     <Container>
-      <Anchor href="/about">About me</Anchor>
+      <p>⬆️ Open the menu, or</p>
+      <ul role="navigation">
+        <li>
+          <Anchor href="/posts">Blog</Anchor>
+        </li>
+        <li>
+          <Anchor href="/about">About</Anchor>
+        </li>
+        <li>
+          <Anchor href="/docs/Web">Web docs</Anchor>
+        </li>
+      </ul>
+      <Heading>Under construction</Heading>
     </Container>
   )
 }

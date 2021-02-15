aa2db955fae853b06abeb0604fe6056c98314512
alattalatta <urty5656@gmail.com>
2021-02-15 12:59:53 +0900

    fix h* id count being persistent between routes

--- components/MDX/index.tsx
+++ components/MDX/index.tsx
@@ -2,7 +2,8 @@ import clsx from 'clsx'
 import hydrate from 'next-mdx-remote/hydrate'
 import { MdxRemote } from 'next-mdx-remote/types'
 import dynamic from 'next/dynamic'
-import React from 'react'
+import { useRouter } from 'next/router'
+import React, { createContext, useContext, useEffect, useRef } from 'react'
 
 import { childrenToText } from 'utils/element'
 
@@ -10,6 +11,21 @@ import { Anchor } from '../basics'
 import Callout, { CalloutCite } from './Callout'
 import { CodeBlock, CodeBlockBad, CodeBlockGood } from './CodeBlock'
 import styles from './styles.module.scss'
+
+const HeadingIDContext = createContext<Map<string, number> | null>(null)
+export const HeadingContext: React.FC = ({ children }) => {
+  const map = useRef(new Map<string, number>()).current
+  const router = useRouter()
+
+  useEffect(() => {
+    const clearMapWhenRouteChanges = () => map.clear()
+    router.events.on('routeChangeStart', clearMapWhenRouteChanges)
+
+    return () => router.events.off('routeChangeStart', clearMapWhenRouteChanges)
+  }, [])
+
+  return <HeadingIDContext.Provider value={map}>{children}</HeadingIDContext.Provider>
+}
 
 /** Non-hydrated MDX contents wrapper. */
 const MDXStatic: React.VFC<{ children: string; className?: string }> = ({ children, className }) => (
@@ -27,7 +43,10 @@ type Props = {
   components: MdxRemote.Components
 }
 export const MDXWrap: React.VFC<Props> = ({ children, className, components }) => {
-  const content = hydrate(children, { components }) as React.ReactElement
+  const content = hydrate(children, {
+    components,
+    provider: { component: HeadingContext, props: {} },
+  }) as React.ReactElement
 
   // pretty lame but uh
   // should properly handle when https://github.com/hashicorp/next-mdx-remote/issues/88 gets resolved
@@ -38,20 +57,20 @@ export const MDXWrap: React.VFC<Props> = ({ children, className, components }) =
   )
 }
 
-// prevent duplicate id
-const HEADING_ID_MAP = new Map<string, number>()
 const headingOf = (level: 2 | 3 | 4): React.FC => {
   const H = `h${level}` as const
   const className = styles[`heading${level}`]
 
   return ({ children }) => {
+    const headingIDContext = useContext(HeadingIDContext)
+
     const textContent = childrenToText(children)
       .replace(/\s/g, '-')
       .replace(/[!@#$%^&*()=+~`'"/\\?.,<>[\]|{}]/g, '')
 
-    const count = HEADING_ID_MAP.get(textContent) || 0
+    const count = headingIDContext?.get(textContent) || 0
     const id = count ? `${textContent}-${count}` : textContent
-    HEADING_ID_MAP.set(textContent, count + 1)
+    headingIDContext?.set(textContent, count + 1)
 
     return (
       <H className={className} id={id}>

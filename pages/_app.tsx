44588a1305cff1fa1985378decb3887f650694b0
alattalatta <urty5656@gmail.com>
2021-02-20 00:48:50 +0900

    add og:url

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 
 import { Page } from 'utils/types'
 
 import 'styles/reset.css'
 
 function App({ Component, pageProps }: { Component: Page; pageProps: Record<string, unknown> }): JSX.Element {
+  const router = useRouter()
+
   const getLayout = Component.getLayout || ((a) => a)
 
   return getLayout(
@@ -15,6 +18,7 @@ function App({ Component, pageProps }: { Component: Page; pageProps: Record<stri
         <meta key="og:type" property="og:type" content="website" />
         <meta key="og:title" property="og:title" content="Sorto.me" />
         <meta key="og:description" property="og:description" content="Sorto.me - Blog/Web Docs" />
+        <meta key="og:url" property="og:url" content={`https://sorto.me${router.asPath}`} />
       </Head>
       <Component {...pageProps} />
     </>,

59777928a4757cd2b7dd66e6f0140b85a1667d02
alattalatta <urty5656@gmail.com>
2021-02-20 18:59:17 +0900

    replace PropOf with React-included one

--- utils/types.ts
+++ utils/types.ts
@@ -3,5 +3,3 @@ import { NextPage } from 'next'
 export type Page<P = Record<string, unknown>> = NextPage<P> & {
   getLayout?: (page: JSX.Element) => JSX.Element
 }
-
-export type PropOf<C> = C extends React.FC<infer P> ? P : never

d29775fa9397d1417dddfd8fa74561bbf20e69f0
alattalatta <urty5656@gmail.com>
2021-02-20 19:09:30 +0900

    add ga

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -27,6 +27,14 @@ export default class Document extends NextDocument {
             href="https://fonts.googleapis.com/css2?family=Nanum+Gothic+Coding:wght@400;700&family=Nanum+Gothic:wght@400;700&display=swap"
             rel="stylesheet"
           />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-7F0E6D3XE2" />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','${
+                process.env.NEXT_PUBLIC_ANALYTICS_ID
+              }',{debug_mode:${process.env.NODE_ENV !== 'production'}});`,
+            }}
+          />
         </Head>
         <body>
           <Main />

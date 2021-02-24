324a7a5a3593c0950eabab70b794ef73ba4ea2bb
alattalatta <urty5656@gmail.com>
2021-02-24 23:01:05 +0900

    more interactiveness for top demo

--- components/DocsMDX/LiveExample/index.tsx
+++ components/DocsMDX/LiveExample/index.tsx
@@ -1,17 +1,25 @@
 import dynamic from 'next/dynamic'
 
-import { LiveCodeProps } from '../LiveCode'
+import { useCodeBlockGroup } from 'hooks/MDX/useCodeBlockGroup'
+
 import styles from './styles.module.scss'
 
 const LiveCode = dynamic(() => import('../LiveCode'))
 
-const LiveExample: React.VFC<LiveCodeProps> = ({ height = 240, name, title = '예제' }) => {
+type Props = {
+  height?: number
+  name: string
+}
+
+const LiveExample: React.VFC<Props> = ({ height = 240, name }) => {
+  const { css: cssTexts, html: htmlTexts, js: jsTexts } = useCodeBlockGroup(name)
+
   return (
     <figure className={styles.root}>
       <figcaption className={styles.caption}>실행 결과</figcaption>
       <div className={styles.container} style={{ height: `${height}px` }}>
         <div className={styles.resultWrap}>
-          <LiveCode height={height} name={name} title={title} />
+          <LiveCode data={{ htmls: htmlTexts, scripts: jsTexts, styles: cssTexts }} height={height} />
         </div>
       </div>
     </figure>

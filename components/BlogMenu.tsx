a4872d52a6e07242f7e349fdcf1c51580ce06d25
alattalatta <urty5656@gmail.com>
2021-03-04 00:11:11 +0900

    accessibility audit

--- components/BlogMenu.tsx
+++ components/BlogMenu.tsx
@@ -18,16 +18,16 @@ const BlogMenu: React.VFC = () => {
   return (
     <Container>
       <SearchField placeholder="블로그 검색..." targetUrl="/posts/search" />
-      <BaseMenu css={{ marginTop: 48 }} aria-label="블로그 링크">
-        <li>
+      <BaseMenu css={{ marginTop: 48 }} role="group" aria-label="블로그 링크">
+        <li role="menuitem">
           <BaseMenuItem href="/posts">Blog</BaseMenuItem>
         </li>
-        <li>
+        <li role="menuitem">
           <BaseMenuItem href="/about">About</BaseMenuItem>
         </li>
       </BaseMenu>
-      <BaseMenu css={{ marginTop: 72 }} aria-label="다른 곳으로 이동">
-        <li>
+      <BaseMenu css={{ marginTop: 72 }} role="group" aria-label="다른 곳으로 이동">
+        <li role="menuitem">
           <BaseIntraNavItem href="/docs">Docs</BaseIntraNavItem>
         </li>
       </BaseMenu>

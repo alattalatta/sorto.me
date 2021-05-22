53dc953e622ebe203c67d0663458a849ed952cbe
alattalatta <urty5656@gmail.com>
2021-05-23 00:21:42 +0900

    css: font-face

--- next.config.js
+++ next.config.js
@@ -1,35 +1,41 @@
 /* eslint-disable no-undef */
 /* eslint-disable @typescript-eslint/no-var-requires */
-
 const withImages = require('next-images')
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
 
-module.exports = withImages({
-  future: {
-    webpack5: true,
-  },
-  async headers() {
-    return [
-      {
-        source: '/assets/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Origin', value: 'https://sorto.me' },
-          { key: 'Vary', value: 'Origin' },
-        ],
-      },
-    ]
-  },
-  async redirects() {
-    return [
-      {
-        source: '/post',
-        destination: '/posts',
-        permanent: true,
-      },
-      {
-        source: '/post/:slug',
-        destination: '/posts/:slug',
-        permanent: true,
-      },
-    ]
-  },
-})
+module.exports = (phase) =>
+  withImages({
+    future: {
+      webpack5: true,
+    },
+    async headers() {
+      console.log(phase)
+      if (phase === PHASE_DEVELOPMENT_SERVER) {
+        return []
+      }
+
+      return [
+        {
+          source: '/assets/:path*',
+          headers: [
+            { key: 'Access-Control-Allow-Origin', value: 'https://sorto.me' },
+            { key: 'Vary', value: 'Origin' },
+          ],
+        },
+      ]
+    },
+    async redirects() {
+      return [
+        {
+          source: '/post',
+          destination: '/posts',
+          permanent: true,
+        },
+        {
+          source: '/post/:slug',
+          destination: '/posts/:slug',
+          permanent: true,
+        },
+      ]
+    },
+  })

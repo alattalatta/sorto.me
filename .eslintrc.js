18bfa0e03510f9beddc7b84d10690a14571061c4
alattalatta <alattalatta@sorto.me>
2022-02-05 23:32:36 +0900

    move linters to repo root

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,19 @@ require('@alattalatta/eslint-config/patch')
 module.exports = {
   extends: ['@alattalatta/eslint-config'],
   parserOptions: {
-    project: './tsconfig.json',
+    project: [
+      './apps/*/tsconfig.json',
+      './packages/*/tsconfig.json',
+    ],
     tsconfigRootDir: __dirname,
   },
   settings: {
     'import/resolver': {
       typescript: {
-        project: process.env.NODE_ENV === 'production' ? './tsconfig.json' : 'apps/web/tsconfig.json',
+        project: [
+          './apps/*/tsconfig.json',
+          './packages/*/tsconfig.json',
+        ]
       },
     },
   },

02d171ce17849894a94990419090e36c0eff8296
alattalatta <alattalatta@sorto.me>
2022-02-06 22:35:01 +0900

    mdx: apply codeblock meta to pre tag as attrs

--- packages/mdx/compile.tsx
+++ packages/mdx/compile.tsx
@@ -1,13 +1,17 @@
 import { compile as compileMDX } from '@mdx-js/mdx'
+import * as O from 'fp-ts/lib/Option'
+import { fst, mapFst, mapSnd } from 'fp-ts/lib/ReadonlyTuple'
+import { flow, pipe } from 'fp-ts/lib/function'
+import type { Element, Properties, Root } from 'hast'
 import rehypeHighlight from 'rehype-highlight'
 import type { Plugin } from 'unified'
-import { remove } from 'unist-util-remove'
+import findAncestor from 'unist-util-ancestor'
+import { visit } from 'unist-util-visit'
 
 async function compile(source: string): Promise<string> {
   const compiled = await compileMDX(source, {
     outputFormat: 'function-body',
-    rehypePlugins: [rehypeHighlight],
-    remarkPlugins: [removeImportsExportsPlugin],
+    rehypePlugins: [rehypeHighlight, metaAttribute],
   })
 
   return String(compiled)
@@ -15,7 +19,29 @@ async function compile(source: string): Promise<string> {
 
 export { compile }
 
-function removeImportsExportsPlugin(): Plugin {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-  return (tree) => remove(tree, 'mdxjsEsm')
+const ensureCodeAndMeta = (element: Element): O.Option<readonly [Element, string]> =>
+  element.tagName === 'code' && element.data?.meta ? O.some([element, element.data.meta as string]) : O.none
+
+const parseMeta = (meta: string): Properties =>
+  meta
+    .split(' ')
+    .map((it) => it.split('=') as [string, string])
+    // preserve hidden, convert others as data attributes
+    .reduce((acc, [key, value]) => ({ ...acc, [key === 'hidden' ? key : `data-${key}`]: value ?? true }), {})
+
+const metaAttribute: Plugin<void[], Root> = () => {
+  return (tree) => {
+    visit(tree, 'element', (node) => {
+      pipe(
+        ensureCodeAndMeta(node),
+        O.map(mapFst((code) => findAncestor(tree, [code]) as Element)),
+        O.filter(flow(fst, (el) => el.tagName === 'pre')),
+        O.map(mapSnd(parseMeta)),
+        O.fold(
+          () => undefined,
+          ([el, props]) => (el.properties = props),
+        ),
+      )
+    })
+  }
 }

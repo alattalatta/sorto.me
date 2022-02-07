f14a03636fd18a72a1334dfe4eeef7c4a568d486
alattalatta <alattalatta@sorto.me>
2022-02-07 17:41:06 +0900

    use lighter eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,15 @@
 require('@alattalatta/eslint-config/patch')
 
 module.exports = {
-  extends: ['@alattalatta/eslint-config'],
+  extends: [process.env.NODE_ENV === 'production' ? '@alattalatta/eslint-config/full' : '@alattalatta/eslint-config'],
   parserOptions: {
-    project: [
-      './apps/*/tsconfig.json',
-      './packages/*/tsconfig.json',
-    ],
+    project: ['./apps/*/tsconfig.json', './packages/*/tsconfig.json'],
     tsconfigRootDir: __dirname,
   },
   settings: {
     'import/resolver': {
       typescript: {
-        project: [
-          './apps/*/tsconfig.json',
-          './packages/*/tsconfig.json',
-        ]
+        project: ['./apps/*/tsconfig.json', './packages/*/tsconfig.json'],
       },
     },
   },

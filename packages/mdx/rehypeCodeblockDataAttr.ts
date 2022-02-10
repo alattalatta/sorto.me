853c37c1160d0fb9ba0e57dd8b7ea56f2cf87b19
alattalatta <alattalatta@sorto.me>
2022-02-10 23:38:07 +0900

    mdx: pass code classes to pre element as well

--- packages/mdx/rehypeCodeblockDataAttr.ts
+++ packages/mdx/rehypeCodeblockDataAttr.ts
@@ -1,13 +1,7 @@
-import * as O from 'fp-ts/lib/Option'
-import { fst, mapFst, mapSnd } from 'fp-ts/lib/ReadonlyTuple'
-import { flow, pipe } from 'fp-ts/lib/function'
 import type { Element, Properties, Root } from 'hast'
 import type { Plugin } from 'unified'
 import findAncestor from 'unist-util-ancestor'
 import { visit } from 'unist-util-visit'
-
-const ensureCodeAndMeta = (element: Element): O.Option<readonly [Element, string]> =>
-  element.tagName === 'code' && element.data?.meta ? O.some([element, element.data.meta as string]) : O.none
 
 const parseMeta = (meta: string): Properties =>
   meta
@@ -19,16 +13,19 @@ const parseMeta = (meta: string): Properties =>
 const rehypeCodeblockDataAttr: Plugin<void[], Root> = () => {
   return (tree) => {
     visit(tree, 'element', (node) => {
-      pipe(
-        ensureCodeAndMeta(node),
-        O.map(mapFst((code) => findAncestor(tree, [code]) as Element)),
-        O.filter(flow(fst, (el) => el.tagName === 'pre')),
-        O.map(mapSnd(parseMeta)),
-        O.fold(
-          () => undefined,
-          ([el, props]) => (el.properties = props),
-        ),
-      )
+      if (node.tagName !== 'code') {
+        return
+      }
+
+      const ancestor = findAncestor(tree, [node]) as Element
+      if (ancestor.tagName !== 'pre') {
+        return
+      }
+
+      ancestor.properties = {
+        ...parseMeta((node.data?.meta as string) || ''),
+        className: node.properties?.className,
+      }
     })
   }
 }

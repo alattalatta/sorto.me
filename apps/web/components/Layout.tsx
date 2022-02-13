eeeacb10f3198641a92387f4feb3f04dcb36f488
alattalatta <alattalatta@sorto.me>
2022-02-13 22:32:41 +0900

    add missing safe-area-inset to layout

--- apps/web/components/Layout.tsx
+++ apps/web/components/Layout.tsx
@@ -5,7 +5,7 @@ import { NavBar } from './NavBar'
 const Body = styled('div', {
   maxWidth: `${982 / 16}rem`,
   margin: `1.5rem auto ${FIXED_STRIP_HEIGHT}rem`,
-  padding: '0 1rem',
+  padding: '0 1rem env(safe-area-inset-bottom)',
 })
 
 const Layout: React.FC = ({ children }) => {

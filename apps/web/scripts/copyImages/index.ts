ef93992ba937ff541b0aef179c498b54de590a3f
alattalatta <alattalatta@sorto.me>
2022-02-18 23:53:15 +0900

    an attempt to fix EEXIST

--- apps/web/scripts/copyImages/index.ts
+++ apps/web/scripts/copyImages/index.ts
@@ -5,10 +5,9 @@ import { copy } from 'fs-extra'
 const packageRoot = path.resolve(__dirname, '../..')
 const contentsRoot = path.resolve(__dirname, '../../../../packages/@contents')
 
-Promise.all([
-  copy(path.join(contentsRoot, 'docs/public'), path.join(packageRoot, 'public'), { overwrite: true }),
-  copy(path.join(contentsRoot, 'posts/public'), path.join(packageRoot, 'public'), { overwrite: true }),
-]).catch((err) => {
-  console.error(err)
-  process.exit(1)
-})
+copy(path.join(contentsRoot, 'docs/public'), path.join(packageRoot, 'public'))
+  .then(() => copy(path.join(contentsRoot, 'posts/public'), path.join(packageRoot, 'public')))
+  .catch((err) => {
+    console.error(err)
+    process.exit(1)
+  })

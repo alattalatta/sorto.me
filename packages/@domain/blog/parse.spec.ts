35b62759beb66f9e8a663a175732b75309a1df99
alattalatta <alattalatta@sorto.me>
2022-03-31 23:21:45 +0900

    remove jest, fiddle with ava

--- packages/@domain/blog/parse.spec.ts
+++ packages/@domain/blog/parse.spec.ts
@@ -1,44 +1,44 @@
 import path from 'path'
+import url from 'url'
+
+import test from 'ava'
 
 import { parse } from './parse'
-import type { PostMetadata } from './types'
 
-describe('Post parser', () => {
-  describe('parse', () => {
-    it('can parse post', async () => {
-      const filePath = path.resolve(__dirname, 'mocks/2020-02-03--full.mdx')
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
 
-      const {
-        content,
-        meta: { updated, ...meta },
-      } = await parse(filePath)
+test('can parse post', async (t) => {
+  const filePath = path.resolve(__dirname, 'mocks/2020-02-03--full.mdx')
 
-      expect(content.trim()).toBe('ccc')
-      expect(meta).toEqual<Omit<PostMetadata, 'updated'>>({
-        created: '2020-02-03',
-        description: 'front matter description &amp;',
-        image: '/images/1999-01-01/image.jpg',
-        slug: '2020-02-03--full',
-        title: 'front matter title',
-      })
-    })
+  const {
+    content,
+    meta: { updated, ...meta },
+  } = await parse(filePath)
 
-    it('can parse post with minimal data', async () => {
-      const filePath = path.resolve(__dirname, 'mocks/2020-02-04--minimal.mdx')
+  t.is(content.trim(), 'ccc')
+  t.deepEqual(meta, {
+    created: '2020-02-03',
+    description: 'front matter description &amp;',
+    image: '/images/1999-01-01/image.jpg',
+    slug: '2020-02-03--full',
+    title: 'front matter title',
+  })
+})
 
-      const {
-        content,
-        meta: { updated, ...meta },
-      } = await parse(filePath)
+test('can parse post with minimal data', async (t) => {
+  const filePath = path.resolve(__dirname, 'mocks/2020-02-04--minimal.mdx')
 
-      expect(content.trim()).toBe('')
-      expect(meta).toEqual<Omit<PostMetadata, 'updated'>>({
-        created: '2020-02-04',
-        description: null,
-        image: '/images/default.jpg',
-        slug: '2020-02-04--minimal',
-        title: 'title',
-      })
-    })
+  const {
+    content,
+    meta: { updated, ...meta },
+  } = await parse(filePath)
+
+  t.is(content.trim(), '')
+  t.deepEqual(meta, {
+    created: '2020-02-04',
+    description: null,
+    image: '/images/default.jpg',
+    slug: '2020-02-04--minimal',
+    title: 'title',
   })
 })

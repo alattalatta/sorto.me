a7c2831ec04c1f4f0445d86ab57249ee24ac6747
alattalatta <alattalatta@sorto.me>
2022-04-02 14:46:28 +0900

    extract doc description from its content

--- packages/@domain/docs/parse.ts
+++ packages/@domain/docs/parse.ts
@@ -1,12 +1,21 @@
-import { filePath, readLastModified } from '@lib/functions/server'
-import { escapeUTF8 } from 'entities'
-import matter from 'gray-matter'
 import fs from 'node:fs/promises'
 import path from 'node:path'
+
+import { filePath, readLastModified } from '@lib/functions/server'
+import { escapeUTF8 } from 'entities'
+import type { GrayMatterFile } from 'gray-matter'
+import matter from 'gray-matter'
+import { remark } from 'remark'
+import stripMarkdown from 'strip-markdown'
+import { select } from 'unist-util-select'
 
 import type { Doc, DocMetadata } from './types'
 
 const __dirname = filePath(import.meta.url)
+
+const processor = remark()
+  .use(() => (root) => select('paragraph', root) || root.children[0] || root)
+  .use(stripMarkdown)
 
 /**
  * Parses a MDX post file. Creation date is parsed from the file's name.
@@ -20,14 +29,34 @@ async function parse(docPath: string, rootDir?: string): Promise<Doc> {
 
   const {
     content,
-    data: { description, excerpt, ...data },
-  } = matter(await sourceAsync)
+    data: { description: descFromMatter, ...data },
+    excerpt,
+  } = matter(await sourceAsync, {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    excerpt: ((file: GrayMatterFile<Buffer>) => (file.excerpt = file.content)) as any,
+  })
+
+  const description = await (async () => {
+    if (descFromMatter === null) {
+      return null
+    }
+
+    if (descFromMatter) {
+      return escapeUTF8(descFromMatter)
+    }
+
+    if (excerpt) {
+      return escapeUTF8(await processor.process(excerpt).then((res) => String(res).trim()))
+    }
+
+    return null
+  })()
 
   return {
     content,
     meta: {
       ...(data as DocMetadata),
-      description: typeof description === 'string' ? escapeUTF8(description) : null,
+      description,
       slug: path.relative(rootDir || __dirname, docPath).replace(/(^mdx\/)|(.mdx$)/gi, ''),
       updated: (await lastModifiedAsync).toISOString(),
     } as DocMetadata,

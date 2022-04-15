29b614b73e9dac0e9976ad185b69a3037a0e5432
alattalatta <alattalatta@sorto.me>
2022-04-15 19:40:44 +0900

    fix search result style

--- packages/@domain/search/index.global.css.ts
+++ packages/@domain/search/index.global.css.ts
@@ -2,11 +2,22 @@ import { colors } from '@lib/ui/theme.css'
 import { globalStyle } from '@vanilla-extract/css'
 
 globalStyle('main .gsc-control-cse', {
+  background: 'none',
+  border: 'none',
   marginTop: '1.5rem',
   padding: 0,
 })
 
+globalStyle('main .gsc-above-wrapper-area', {
+  borderBottom: 'none',
+})
+
+globalStyle('main .gsc-control-cse .gsc-results', {
+  width: '100%',
+})
+
 globalStyle('main .gsc-control-cse .gsc-result', {
+  border: 'none',
   padding: '0.75rem 0',
 })
 

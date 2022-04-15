1f3729b2468b51c583a3fbfd7b7ff7b4a2a93265
alattalatta <alattalatta@sorto.me>
2022-04-15 23:24:53 +0900

    make /frame a static page

--- apps/web/pages/frame.tsx
+++ apps/web/pages/frame.tsx
@@ -1,16 +1,3 @@
 import { FramePage } from '@domain/docs'
-import type { GetServerSideProps } from 'next'
 
 export default FramePage
-
-export const getServerSideProps: GetServerSideProps = ({ req }) => {
-  if (process.env.NODE_ENV === 'production' && !req.headers.referer?.startsWith('https://sorto.me')) {
-    return Promise.resolve({
-      notFound: true,
-    })
-  }
-
-  return Promise.resolve({
-    props: {},
-  })
-}

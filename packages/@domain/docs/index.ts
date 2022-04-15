e6ae7b6af16474cbfc2e54b9bd6c8ce8e1ce8807
alattalatta <alattalatta@sorto.me>
2022-04-15 23:14:14 +0900

    fix missing type exports

--- packages/@domain/docs/index.ts
+++ packages/@domain/docs/index.ts
@@ -2,6 +2,6 @@ export { DocPage } from './DocPage'
 export type { DocPageProps } from './DocPage'
 
 export { FramePage } from './FramePage'
-export type { FramePageProps } from './FramePage'
 
+export type { Doc, DocMetadata } from './types'
 export { getCompatData } from './utils'

752fae6246e97890a5c36deadb2bf18697f0608c
alattalatta <alattalatta@sorto.me>
2022-05-26 23:30:33 +0900

    highlight fragment when hash changes

--- packages/@lib/ui/src/Layout.css.ts
+++ packages/@lib/ui/src/Layout.css.ts
@@ -1,9 +1,23 @@
-import { style } from '@vanilla-extract/css'
+import { keyframes, style } from '@vanilla-extract/css'
 
 import { FIXED_STRIP_HEIGHT } from '../fixedStrip.css'
+import { colors } from '../theme.css'
 
 export const body = style({
   maxWidth: `${982 / 16}rem`,
   margin: `1.5rem auto ${(FIXED_STRIP_HEIGHT + 24) / 16}rem`,
   paddingBottom: 'env(safe-area-inset-bottom)',
+})
+
+const marker = keyframes({
+  '0%': {
+    background: colors.warn,
+  },
+  '100%': {
+    background: 'transparent',
+  },
+})
+
+export const hashMatched = style({
+  animation: `${marker} 1s ease`,
 })
--- packages/@lib/ui/src/Layout.tsx
+++ packages/@lib/ui/src/Layout.tsx
@@ -1,8 +1,62 @@
+import { useEffect } from 'react'
+import { colors } from '../theme.css'
 import * as styles from './Layout.css'
 import { NavBar } from './NavBar'
 import { ScrollBack } from './ScrollBack'
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  // highlight fragment the hash points
+  useEffect(() => {
+    const highlightFragment = (): void => {
+      const hash = decodeURIComponent(location.hash.slice(1))
+      if (!hash) {
+        return
+      }
+
+      const target = (() => {
+        const element = document.getElementById(hash)
+        if (!element) {
+          return
+        }
+
+        const anchor = element.querySelector('a a')
+        return anchor || element
+      })()
+      if (!target) {
+        return
+      }
+
+      const playingAnimation = target.getAnimations()[0]
+      if (playingAnimation) {
+        playingAnimation.cancel()
+      }
+
+      target.animate(
+        [
+          {
+            background: colors.highlight,
+          },
+          {
+            background: 'transparent',
+          },
+        ],
+        {
+          duration: 1000,
+          easing: 'ease',
+        },
+      )
+    }
+
+    // highlight on page load
+    const initialHighlight = window.setTimeout(highlightFragment, 1000)
+
+    window.addEventListener('hashchange', highlightFragment)
+    return () => {
+      window.clearTimeout(initialHighlight)
+      window.removeEventListener('hashchange', highlightFragment)
+    }
+  }, [])
+
   return (
     <>
       <NavBar />

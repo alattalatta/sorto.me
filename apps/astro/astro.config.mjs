b6cf5d9c5d70cbdf25ade6398ab22b986d8ed7fc
alattalatta <alattalatta@sorto.me>
2024-06-29 19:48:54 +0900

    feat(astro): Content body

--- apps/astro/astro.config.mjs
+++ apps/astro/astro.config.mjs
@@ -1,9 +1,16 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
 
-import react from "@astrojs/react";
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), react()]
-});
+  integrations: [
+    mdx({
+      shikiConfig: {
+        theme: 'catppuccin-latte',
+      },
+    }),
+    react(),
+  ],
+})

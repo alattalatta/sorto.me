8c75ca7759862f701c0a37547cf58314f032a1c8
alattalatta <alattalatta@sorto.me>
2024-07-13 03:41:35 +0900

    feat(astro): Restore table/deflist styles and ids

--- packages/@lib/mdx/src/remarkRehypeHandlers.ts
+++ packages/@lib/mdx/src/remarkRehypeHandlers.ts
@@ -4,25 +4,25 @@ import type { Handlers } from 'mdast-util-to-hast'
 export const remarkRehypeHandlers: Handlers = {
   definitionList: (state, node: DefinitionList) => ({
     children: state.all(node),
-    properties: {},
+    properties: { ...node.data?.hProperties },
     type: 'element',
     tagName: 'dl',
   }),
   notebox: (state, node: Notebox) => ({
     children: state.all(node),
-    properties: { className: `notebox notebox-${node.severity}` },
+    properties: { ...node.data?.hProperties, className: `notebox notebox-${node.severity}` },
     type: 'element',
     tagName: 'div',
   }),
   term: (state, node: Term) => ({
     children: state.all(node),
-    properties: {},
+    properties: { ...node.data?.hProperties },
     type: 'element',
     tagName: 'dt',
   }),
   termDescription: (state, node: TermDescription) => ({
     children: state.all(node),
-    properties: {},
+    properties: { ...node.data?.hProperties },
     type: 'element',
     tagName: 'dd',
   }),

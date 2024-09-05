5aae75987ad676df1e176ee66a04bdf8c6b43e65
alattalatta <alattalatta@sorto.me>
2024-09-06 01:26:27 +0900

    feat(mdx): Open external links in a new tab

--- packages/mdx/src/remarkRehypeHandlers.ts
+++ packages/mdx/src/remarkRehypeHandlers.ts
@@ -1,5 +1,6 @@
-import type { DefinitionList, Notebox, Term, TermDescription } from 'mdast'
-import type { Handlers } from 'mdast-util-to-hast'
+import type { Element } from 'hast'
+import type { DefinitionList, Link, LinkReference, Notebox, Term, TermDescription } from 'mdast'
+import { defaultHandlers, type Handlers } from 'mdast-util-to-hast'
 
 export const remarkRehypeHandlers: Handlers = {
   definitionList: (state, node: DefinitionList) => ({
@@ -8,6 +9,31 @@ export const remarkRehypeHandlers: Handlers = {
     type: 'element',
     tagName: 'dl',
   }),
+  link: (state, node: Link) => {
+    const fromDefault = defaultHandlers.link(state, node)
+    const href = String(fromDefault.properties.href)
+    if (href.startsWith('http')) {
+      fromDefault.properties.target = '_blank'
+      fromDefault.properties.rel = 'noopener'
+    }
+
+    return fromDefault
+  },
+  linkReference: (state, node: LinkReference) => {
+    const fromDefaultRaw = defaultHandlers.linkReference(state, node)
+    if (Array.isArray(fromDefaultRaw)) {
+      return fromDefaultRaw
+    }
+
+    const fromDefault = fromDefaultRaw as unknown as Element
+    const href = String(fromDefault.properties.href)
+    if (href.startsWith('http')) {
+      fromDefault.properties.target = '_blank'
+      fromDefault.properties.rel = 'noopener'
+    }
+
+    return fromDefault
+  },
   notebox: (state, node: Notebox) => ({
     children: state.all(node),
     properties: { ...node.data?.hProperties, className: `notebox notebox-${node.severity}` },

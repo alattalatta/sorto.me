a70cd84055eed5d4f257dc81f85a932a6222648b
alattalatta <alattalatta@sorto.me>
2025-06-10 20:59:23 +0900

    docs(video): Update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,16 +2,36 @@ import react from '@alattalatta/eslint-config/react'
 import astro from 'eslint-plugin-astro'
 import tselint from 'typescript-eslint'
 
-export default tselint.config(react, ...astro.configs.recommended, {
-  languageOptions: {
-    parserOptions: {
-      project: true,
-      projectService: true,
-      tsconfigRootDir: import.meta.dirname,
+export default tselint.config(
+  react,
+  ...astro.configs.recommended.map((c) => {
+    const opts = c.languageOptions
+    const baseParser = opts?.parser?.meta?.name
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    const subParser = opts?.parserOptions?.parser?.meta?.name
+
+    if (opts && (baseParser === ts || subParser === ts)) {
+      opts.parserOptions = {
+        ...opts.parserOptions,
+        projectService: false,
+        project: true,
+      }
+    }
+
+    return c
+  }),
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ['*.astro'],
+        },
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      'astro/sort-attributes': 'warn',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
     },
   },
-  rules: {
-    'astro/sort-attributes': 'warn',
-    '@typescript-eslint/no-unsafe-assignment': 'off',
-  },
-})
+)
